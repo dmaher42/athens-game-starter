8467399ce14ca2e9e006668f5e04a049d92fa8ad
dmaher42 <Daniel.maher601@schools.sa.edu.au>
2025-12-21 14:47:29 +1030

    Increase default radius in createSkyDome function

--- src/world/skybox/SkyDome.js
+++ src/world/skybox/SkyDome.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 
-export function createSkyDome(texture, radius = 800) {
+export function createSkyDome(texture, radius = 1600) {
   const geo = new THREE.SphereGeometry(radius, 64, 32);
 
   const mat = new THREE.MeshBasicMaterial({
