4113a60abc4665c673134053650a688137a5deb9
dmaher42 <dmaher42@users.noreply.github.com>
2026-01-01 00:28:54 +0000

    deploy: 912b137fdce3869548b571521eca4a5e662d8d14

--- assets/index-ChZRhts2.js
+++ assets/index-ChZRhts2.js
@@ -43675,6 +43675,7 @@ function bindGroundTexture(material, label, url, repeat) {
       console.log(`[Ground] ✅ ${label} texture applied to material`, {
         materialHasMap: !!material.map,
         mapIsValid: material.map?.image?.width > 0,
+        materialColor: material.color?.getHexString(),
         repeat,
         wrapS: material.map?.wrapS,
         wrapT: material.map?.wrapT
@@ -59603,7 +59604,7 @@ function resolveKTX2TranscoderPath() {
 }
 async function createKTX2Loader(renderer2) {
   const { KTX2Loader } = await __vitePreload(async () => {
-    const { KTX2Loader: KTX2Loader2 } = await import("./KTX2Loader-C5rh44rj.js");
+    const { KTX2Loader: KTX2Loader2 } = await import("./KTX2Loader-CKnN2HIj.js");
     return { KTX2Loader: KTX2Loader2 };
   }, true ? [] : void 0);
   const loader = new KTX2Loader();
@@ -60338,7 +60339,7 @@ class GLTFMaterialsPbrSpecularGlossinessExtension {
 }
 async function createGLTFLoader(renderer2) {
   const { GLTFLoader } = await __vitePreload(async () => {
-    const { GLTFLoader: GLTFLoader2 } = await import("./GLTFLoader-DKazTh0S.js");
+    const { GLTFLoader: GLTFLoader2 } = await import("./GLTFLoader-VK2QkmR0.js");
     return { GLTFLoader: GLTFLoader2 };
   }, true ? [] : void 0);
   const loader = new GLTFLoader();
@@ -61286,8 +61287,8 @@ const DEFAULT_ENGINE_CONFIG = ({
     baseUrl: baseUrl2,
     queryParams,
     build: {
-      time: true ? "2026-01-01T00:23:37.398Z" : "",
-      sha: true ? "0b55982ed8be4077e4297b2c395d6f27829c999c" : ""
+      time: true ? "2026-01-01T00:28:38.734Z" : "",
+      sha: true ? "912b137fdce3869548b571521eca4a5e662d8d14" : ""
     },
     districtRuleCandidates: buildDistrictRuleUrlCandidates(baseUrl2),
     featureFlags: {
@@ -71933,4 +71934,4 @@ export {
   Material as y,
   LineBasicMaterial as z
 };
-//# sourceMappingURL=index-ZRBDqQue.js.map
+//# sourceMappingURL=index-ChZRhts2.js.map
